3dfe27b1593cab6b05dceac0fb97274d2cda13de
BruBraga <57160980+BruBraga@users.noreply.github.com>
2026-02-08 22:24:27 -0300

    atual3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,7 +65,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/logo_Daps.png', //criar um card pra compartilhamento?//
+      image: 'img/docusaurus-social-card.png', //criar um card pra compartilhamento?//
       colorMode: {
         respectPrefersColorScheme: true,
       },
